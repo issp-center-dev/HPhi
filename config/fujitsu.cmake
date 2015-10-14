2fd0a733ebbb659d7e168e6256f1ae64b141ff35
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2015-10-14 11:48:11 +0900

    Fix configuration for fujitsu commpiler

--- config/fujitsu.cmake
+++ config/fujitsu.cmake
@@ -1,8 +1,7 @@
 # for Fujitsu Compiler
 set(CMAKE_C_COMPILER "fccpx" CACHE STRING "" FORCE)
-set(CMAKE_C_FLAGS_RELEASE "-Kfast,SPARC64IXfx,parallel -Kmemalias,alias_const" CACHE STRING "" FORCE)
+set(CMAKE_C_FLAGS_RELEASE "-DNDEBUG -Kfast,parallel -Kmemalias,alias_const" CACHE STRING "" FORCE)
 set(OpenMP_C_FLAGS "-Kopenmp" CACHE STRING "" FORCE)
 
 # for SSL2
-set(LAPACK_FOUND TRUE CACHE BOOL "" FORCE)
-set(CMAKE_EXE_LINKER_FLAGS "-SSL2 --linkfortran" CACHE STRING "" FORCE)
+set(BLAS_LIBRARIES "-SSL2 --linkfortran" CACHE STRING "" FORCE)
