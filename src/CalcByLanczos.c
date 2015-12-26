9bfa2670de47f470047ce90cef5ef30f836692e7
k-yoshimi <k-yoshimi@issp.u-tokyo.ac.jp>
2015-12-26 15:55:54 +0900

    #288 bugfix for sz

--- src/CalcByLanczos.c
+++ src/CalcByLanczos.c
@@ -84,13 +84,12 @@ int CalcByLanczos(
       break;
     default:
       fclose(fp);
-      return -1;
+      exitMPI(-1);
     }
  
     if(Lanczos_EigenValue(&(X->Bind))!=0){
-      fprintf(stdoutMPI, "Lanczos Eigenvalue is not converged in this process.\n");
-      fclose(fp);
-      return -1;
+      fprintf(stderr, "Lanczos Eigenvalue is not converged in this process.\n");
+      exitMPI(-1);
     }  
     Lanczos_EigenVector(&(X->Bind));
     expec_energy(&(X->Bind));
@@ -147,19 +146,19 @@ int CalcByLanczos(
     sscanf(ctmp2, "%ld \n",&i_max);
     if(i_max != X->Bind.Check.idim_max){
       fprintf(stderr, "Error: A file of Inputvector is incorrect.\n");
-      fclose(fp);
       exitMPI(-1);
     }
     
     i=1;
     while(fgetsMPI(ctmp2, 256, fp) != NULL){
-	  sscanf(ctmp2, "%ld %lf %lf \n",
-		 &_list_1,
-		 &dRealVec,
-		 &dImagVec);	  
-	  v1[i]=dRealVec+I*dImagVec;
-	  i++;
-	}
+      sscanf(ctmp2, "%ld %lf %lf \n",
+	     &_list_1,
+	     &dRealVec,
+	     &dImagVec);	  
+      v1[i]=dRealVec+I*dImagVec;
+      i++;
+    }
+    fclose(fp);
   }
 
   // v1 is eigen vector
@@ -193,29 +192,27 @@ int CalcByLanczos(
   }
   
   if(childfopenMPI(sdt, "w", &fp)!=0){
-    fclose(fp);
-    return -1;
+    exitMPI(-1);
   }  
 
-    fprintf(fp,"Energy  %.10lf \n",X->Bind.Phys.energy);
-    fprintf(fp,"Doublon  %.10lf \n",X->Bind.Phys.doublon);
-    fprintf(fp,"Sz  %.10lf \n",X->Bind.Phys.sz);
-    //    fprintf(fp,"total S^2  %.10lf \n",X->Bind.Phys.s2);
-    
-    fclose(fp);
-
-    if(X->Bind.Def.iOutputEigenVec==TRUE){
-      sprintf(sdt, cFileNameOutputEigen, X->Bind.Def.CDataFileHead, X->Bind.Def.k_exct-1);
-      if(childfopenMPI(sdt, "w", &fp)!=0){
-	fclose(fp);
-	return -1;
-      }
-      fprintf(fp, "%ld \n", X->Bind.Check.idim_max);
-      for(i=1; i<=X->Bind.Check.idim_max; i++){
-	fprintf(fp, "%ld %.10lf %.10lf\n", list_1[i], creal(v1[i]), cimag(v1[i]));
-      }    
+  fprintf(fp,"Energy  %.10lf \n",X->Bind.Phys.energy);
+  fprintf(fp,"Doublon  %.10lf \n",X->Bind.Phys.doublon);
+  fprintf(fp,"Sz  %.10lf \n",X->Bind.Phys.sz);
+  //    fprintf(fp,"total S^2  %.10lf \n",X->Bind.Phys.s2);    
+  
+  if(X->Bind.Def.iOutputEigenVec==TRUE){
+    sprintf(sdt, cFileNameOutputEigen, X->Bind.Def.CDataFileHead, X->Bind.Def.k_exct-1);
+    if(childfopenMPI(sdt, "w", &fp)!=0){
       fclose(fp);
-    }
-    
+      exitMPI(-1);
+      }
+    fprintf(fp, "%ld \n", X->Bind.Check.idim_max);
+    for(i=1; i<=X->Bind.Check.idim_max; i++){
+      fprintf(fp, "%ld %.10lf %.10lf\n", list_1[i], creal(v1[i]), cimag(v1[i]));
+    }    
+    fclose(fp);
+  }
+  
+  fclose(fp);
   return 0;
 }
--- src/sz.c
+++ src/sz.c
@@ -73,6 +73,7 @@ int sz
   long unsigned int num_loc, div_down;
   int num_loc_up;
   int icheck_loc;
+  int ihfSpinDown=0;
   // [e] for Kondo
     
   long unsigned int i_max;
@@ -125,7 +126,7 @@ int sz
   case Spin:
     if(X->Def.iFlgGeneralSpin==FALSE){
       if(GetSplitBitByModel(X->Def.Nsite, X->Def.iCalcModel, &irght, &ilft, &ihfbit)!=0){
-	return -1;
+	exitMPI(-1);
       }
     fprintf(stdoutMPI, "idim=%lf irght=%ld ilft=%ld ihfbit=%ld \n",idim,irght,ilft,ihfbit);
     }
@@ -143,7 +144,7 @@ int sz
 
   if(X->Def.READ==1){
     if(!Read_sz(X, irght, ilft, ihfbit, &i_max)==0){
-      return -1;
+      exitMPI(-1);
     }
   }
   else{ 
@@ -299,14 +300,13 @@ int sz
             
     case Kondo:
       // this part can not be parallelized
-
       N_all_up   = X->Def.Nup;
       N_all_down = X->Def.Ndown;
       fprintf(stdoutMPI, "N_all_up=%d N_all_down=%d \n",N_all_up,N_all_down);
 
       jb = 0;
       num_loc=0;
-      for(j=(X->Def.Nsite+1)/2; j< X->Def.Nsite ;j++){
+      for(j=X->Def.Nsite/2; j< X->Def.Nsite ;j++){
 	if(X->Def.LocSpn[j] != ITINERANT){
 	  num_loc += 1;
 	}
@@ -319,7 +319,7 @@ int sz
 	num_down=0;	
 	icheck_loc=1;
 
-	for(j=(X->Def.Nsite+1)/2; j< X->Def.Nsite ;j++){
+	for(j=X->Def.Nsite/2; j< X->Def.Nsite ;j++){
 	  div_up    = i & X->Def.Tpow[2*j];
 	  div_up    = div_up/X->Def.Tpow[2*j];
 	  div_down  = i & X->Def.Tpow[2*j+1];
@@ -331,7 +331,13 @@ int sz
 	  }else{    
 	    num_up   += div_up;     
 	    num_down += div_down;
-	    icheck_loc   = icheck_loc*(div_up^div_down);// exclude doubllly ocupited site
+	    if(X->Def.Nsite%2==1 && j==(X->Def.Nsite/2)){
+	      icheck_loc= icheck_loc;
+	      ihfSpinDown=div_down;
+	    }
+	    else{
+	      icheck_loc   = icheck_loc*(div_up^div_down);// exclude doubllly ocupited site
+	    }
 	  }
 	}
 
@@ -340,10 +346,31 @@ int sz
 	  all_loc =  X->Def.NLocSpn-num_loc;
 	  all_up   = (X->Def.Nsite+tmp_res)/2-all_loc;
 	  all_down = (X->Def.Nsite-tmp_res)/2-all_loc;
+	  if(X->Def.Nsite%2==1 && X->Def.LocSpn[X->Def.Nsite/2] != ITINERANT){
+	    all_up   = (X->Def.Nsite)/2-all_loc;
+	    all_down = (X->Def.Nsite)/2-all_loc;
+	  }
+	  
 	  for(num_loc_up=0; num_loc_up <= all_loc; num_loc_up++){
 	    tmp_1 = Binomial(all_loc, num_loc_up, comb, all_loc);
-	    tmp_2 = Binomial(all_up, X->Def.Nup-num_up-num_loc_up, comb, all_up);
-	    tmp_3 = Binomial(all_down, X->Def.Ndown-num_down-(all_loc-num_loc_up), comb, all_down);
+	    if( X->Def.Nsite%2==1 && X->Def.LocSpn[X->Def.Nsite/2] != ITINERANT){
+	      if(ihfSpinDown !=0){
+		tmp_2 = Binomial(all_up, X->Def.Nup-num_up-num_loc_up, comb, all_up);
+		tmp_3 = Binomial(all_down, X->Def.Ndown-num_down-(all_loc-num_loc_up), comb, all_down);
+		
+	      }
+	      else{
+		num_up += 1;
+		tmp_2 = Binomial(all_up, X->Def.Nup-num_up-num_loc_up, comb, all_up);
+		tmp_3 = Binomial(all_down, X->Def.Ndown-num_down-(all_loc-num_loc_up), comb, all_down);
+		
+	      }
+	    }
+	    else{
+	      tmp_2 = Binomial(all_up, X->Def.Nup-num_up-num_loc_up, comb, all_up);
+	      tmp_3 = Binomial(all_down, X->Def.Ndown-num_down-(all_loc-num_loc_up), comb, all_down);
+	    }
+	      
 	    jb   += tmp_1*tmp_2*tmp_3;
 	  }
 	}	 
@@ -440,29 +467,10 @@ int sz
 		
 	i_free1(HilbertNumToSz, 2*Max2Sz+1);	
       }
-
-      //debug
-      /*
-      ihfbit=X->Check.sdim;
-      icnt=0;
-      for(j=0; j<X->Def.Tpow[X->Def.Nsite-1]*2; j++){
-	jb=list_2_1[j%ihfbit]+list_2_2[j/ihfbit];
-	if(j==list_1[jb]){
-	  printf("j= %ld, list_1=%ld\n", j, list_1[jb]);
-	  icnt++;
-	}
-      }
-      printf("icnt=%d\n", icnt);
-      exit(1);
-      for(j =1; j<=X->Check.idim_max; j++){
-	printf("j= %ld, list_1=%ld\n", j, list_1[j]);
-      }
-      exit(1);
-      */
       
       break;
     default:
-      return -1;
+      exitMPI(-1);
        
     }    
     i_max=icnt;
@@ -481,11 +489,11 @@ int sz
     fprintf(stdoutMPI, cErrSz_ShowDim, i_max, X->Check.idim_max);
     strcpy(sdt_err,cFileNameErrorSz);
     if(childfopenMPI(sdt_err,"a",&fp_err)!=0){
-      return -1;
+      exitMPI(-1);
     }
     fprintf(fp_err,cErrSz_OutFile);
     fclose(fp_err);
-    return -1;
+    exitMPI(-1);
   }
   
   fprintf(stdoutMPI, "%s", cProEndCalcSz);
@@ -652,7 +660,7 @@ int child_omp_sz_Kondo(long unsigned int ib, long unsigned int ihfbit,int N2,str
   num_up   = 0;
   num_down = 0;
   icheck_loc=1;
-  for(j=(X->Def.Nsite+1)/2; j< X->Def.Nsite ;j++){
+  for(j=X->Def.Nsite/2; j< X->Def.Nsite ;j++){
     div_up    = i & X->Def.Tpow[2*j];
     div_up    = div_up/X->Def.Tpow[2*j];
     div_down  = i & X->Def.Tpow[2*j+1];
@@ -663,8 +671,13 @@ int child_omp_sz_Kondo(long unsigned int ib, long unsigned int ihfbit,int N2,str
       num_down += div_down;  
     }else{    
       num_up   += div_up;        
-      num_down += div_down;  
-      icheck_loc   = icheck_loc*(div_up^div_down);// exclude doubllly ocupited site
+      num_down += div_down;
+      if(X->Def.Nsite%2==1 && j==(X->Def.Nsite/2)){
+	icheck_loc= icheck_loc;
+      }
+      else{
+	icheck_loc   = icheck_loc*(div_up^div_down);// exclude doubllly ocupited site
+      }
     }
   }
   
@@ -689,10 +702,23 @@ int child_omp_sz_Kondo(long unsigned int ib, long unsigned int ihfbit,int N2,str
 	}else{    
 	  num_up   += div_up;        
 	  num_down += div_down;  
-	  icheck_loc   = icheck_loc*(div_up^div_down);// exclude doubllly ocupited site
+	  if(X->Def.Nsite%2==1 && j==(X->Def.Nsite/2)){
+	    icheck_loc= icheck_loc;
+	  }
+	  else{
+	    icheck_loc   = icheck_loc*(div_up^div_down);// exclude doubllly ocupited site
+	  }
 	}
       }
-
+      
+      if(icheck_loc == 1 && X->Def.LocSpn[X->Def.Nsite/2] != ITINERANT && X->Def.Nsite%2==1){
+	div_up    = ia & X->Def.Tpow[X->Def.Nsite-1];
+	div_up    = div_up/X->Def.Tpow[X->Def.Nsite-1];
+	div_down  = (ib*ihfbit) & X->Def.Tpow[X->Def.Nsite];
+	div_down  = div_down/X->Def.Tpow[X->Def.Nsite];
+	icheck_loc= icheck_loc*(div_up^div_down);
+      }
+      
       if(num_up == X->Def.Nup && num_down == X->Def.Ndown && icheck_loc==1){
 	list_1[ja+jb]=ia+ib*ihfbit;
 	list_2_1[ia]=ja;
@@ -905,12 +931,12 @@ int Read_sz
     break;
   }
   if(childfopenMPI(sdt,"r", &fp)!=0){
-    return -1;
+    exitMPI(-1);
   }  
 
   if(fp == NULL){
     if(childfopenMPI(cFileNameErrorSz,"a",&fp_err)!=0){
-      return -1;
+      exitMPI(-1);
     }
     fprintf(fp_err, cErrSz_NoFile);
     fprintf(fp_err, cErrSz_NoFile_Show,sdt);
