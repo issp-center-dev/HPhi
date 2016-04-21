d55fbc25969c31bb50ae0e3941f8f1f49fbdc133
mitsuaki1987 <kawamitsuaki@gmail.com>
2016-04-22 00:30:34 +0900

    #367 Parallelize in generating initial state

--- src/wrapperMPI.c
+++ src/wrapperMPI.c
@@ -36,7 +36,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * @author Mitsuaki Kawamura (The University of Tokyo)
  */
 void InitializeMPI(int argc, char *argv[]){
-  int ierr, nthreads;
+  int ierr;
 
 #ifdef MPI
   ierr = MPI_Init(&argc, &argv);
