2b7f30bde680c009ff6676a0bd739113099240e2
k-yoshimi <k-yoshimi@issp.u-tokyo.ac.jp>
2016-06-23 14:22:18 +0900

    add test for restart double

--- test/checkspectrum_restart_double/calcspec.sh
+++ test/checkspectrum_restart_double/calcspec.sh
@@ -4,5 +4,7 @@ echo "start to calculate eigenvec"
 ./HPhi -e ./namelist_calceigenvec.def > output_eigenvec.out
 echo "start to output information"
 ./HPhi -e ./namelist_outputinfo.def > outputinfo.out
-echo "start to recalculate spectrum"
-./HPhi -e ./namelist_recalc.def > outputrecalc.out
+echo "start to restart spectrum"
+./HPhi -e ./namelist_restart.def > outputrestart.out
+echo "start to restart2 spectrum"
+./HPhi -e ./namelist_restart.def > outputrestart.out
