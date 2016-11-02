cc9eb6b771db172ff202f77cbca2b7e228b5d7a9
k-yoshimi <k-yoshimi@issp.u-tokyo.ac.jp>
2016-11-02 12:43:55 +0900

    modify name of variable

--- src/time.c
+++ src/time.c
@@ -16,13 +16,13 @@
 
 void current_utc_time(struct timespec *ts) {
 #ifdef _OSX // OS X does not have clock_gettime, use clock_get_time
-  clock_serv_t cclock;
-  mach_timespec_t mts;
-  host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
-  clock_get_time(cclock, &mts);
-  mach_port_deallocate(mach_task_self(), cclock);
-  ts->tv_sec = mts.tv_sec;
-  ts->tv_nsec = mts.tv_nsec;
+  clock_serv_t clock_mac;
+  mach_timespec_t mts_mac;
+  host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &clock_mac);
+  clock_get_time(clock_mac, &mts_mac);
+  mach_port_deallocate(mach_task_self(), clock_mac);
+  ts->tv_sec = mts_mac.tv_sec;
+  ts->tv_nsec = mts_mac.tv_nsec;
 #else
   clock_gettime(CLOCK_REALTIME, ts);
 #endif
