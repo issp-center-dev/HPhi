3bc071cdbe0c9100926890ef425d3d3fa333a931
mitsuaki1987 <kawamitsuaki@gmail.com>
2017-03-11 12:19:51 +0900

    Forgotten in the previous commit

--- src/komega/komega_bicg.h
+++ src/komega/komega_bicg.h
@@ -15,7 +15,7 @@ You should have received a copy of the GNU Lesser General Public
 License along with this library; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
-For more details, See ÅeCOPYING.LESSERÅf in the root directory of this library.
+For more details, See 'COPYING.LESSER' in the root directory of this library.
 */
 
 #include <complex.h>
