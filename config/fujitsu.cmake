548ba439c459df5c545dff881560c6b9f9a875af
k-yoshimi <k-yoshimi@issp.u-tokyo.ac.jp>
2017-06-19 16:45:11 +0900

    add compile option for FUJITSU

--- config/fujitsu.cmake
+++ config/fujitsu.cmake
@@ -3,7 +3,7 @@ set(CMAKE_C_COMPILER "mpifccpx" CACHE STRING "" FORCE)
 set(CMAKE_C_FLAGS_RELEASE "-DNDEBUG -Kfast,parallel -Kmemalias,alias_const" CACHE STRING "" FORCE)
 set(OpenMP_C_FLAGS "-Kopenmp" CACHE STRING "" FORCE)
 set(CMAKE_Fortran_COMPILER "mpifrtpx" CACHE STRING "" FORCE)
-set(CMAKE_Fortran_FLAGS_RELEASE "-DNDEBUG -Kfast,parallel" CACHE STRING "" FORCE)
+set(CMAKE_Fortran_FLAGS_RELEASE "-DFUJITSU -DNDEBUG -Kfast,parallel" CACHE STRING "" FORCE)
 set(OpenMP_Fortran_FLAGS "-Kopenmp" CACHE STRING "" FORCE)
 set(MPI_EXTRA_LIBRARY "-lmpi_f90 -lmpi_f77" CACHE STRING "" FORCE)
 
