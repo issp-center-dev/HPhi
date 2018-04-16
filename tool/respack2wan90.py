3ec50ee3cf8fc727f68ca0ec22761155a5df7208
mitsuaki1987 <kawamitsuaki@gmail.com>
2018-04-16 14:56:34 +0900

    Unify to that script in DCore.

--- tool/respack2wan90.py
+++ tool/respack2wan90.py
@@ -38,7 +38,7 @@ def res2wan(name_in, name_out):
     temp1 = [[]]
     nr = 0
     for iline in range(len(line)):
-        if line[iline] == "\n":
+        if line[iline] == "\n" or line[iline] == " \n":
             temp1.append([])
             nr += 1
         else:
@@ -125,15 +125,27 @@ def ref2geom(filename):
             print("%f %f %f" % (centre[iwan, 0], centre[iwan, 1], centre[iwan, 2]), file=fo)
 
 
-args = sys.argv
+def respack2wan90(seedname):
 
-if len(args) != 2:
-    print("\nUsage:\n")
-    print("  $ respack2wan90.py seedname\n")
-    exit(-1)
+    res2wan("./dir-wan/dat.h_mat_r", seedname + "_hr.dat")
+    res2wan("./dir-intW/dat.Wmat", seedname + "_ur.dat")
+    res2wan("./dir-intJ/dat.Jmat", seedname + "_jr.dat")
 
-res2wan("./dir-wan/dat.h_mat_r", args[1] + "_hr.dat")
-res2wan("./dir-intW/dat.Wmat", args[1] + "_ur.dat")
-res2wan("./dir-intJ/dat.Jmat", args[1] + "_jr.dat")
+    ref2geom(seedname + "_geom.dat")
 
-ref2geom(args[1] + "_geom.dat")
+
+if __name__ == '__main__':
+
+    args = sys.argv
+    seedname = "zvo"
+
+    if len(args) == 1:
+        seedname = "zvo"
+    elif len(args) == 2:
+        seedname = args[1]
+    else:
+        print("\nUsage:\n")
+        print("  $ respack2wan90.py [seedname]\n")
+        exit(-1)
+
+    respack2wan90(seedname)
