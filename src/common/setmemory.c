7330022d4e1c42abfa7f6886fbff5f62d19f7c91
tmisawa <tmisawa@issp.u-tokyo.ac.jp>
2019-01-11 23:43:57 +0900

    use calloc, delete memset

--- src/common/setmemory.c
+++ src/common/setmemory.c
@@ -29,8 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /// \author Kazuyoshi Yoshimi (University of Tokyo)
 unsigned int *ui_1d_allocate(const long unsigned int N){
     unsigned int *A;
-    A     = (unsigned int*)malloc((N)*sizeof(unsigned int));
-    memset(A, 0, sizeof(unsigned int)*N);
+    A     = (unsigned int*)calloc((N),sizeof(unsigned int));
     return A;
 }
 
@@ -49,8 +48,7 @@ void free_ui_1d_allocate(unsigned int *A){
 /// \author Kazuyoshi Yoshimi (University of Tokyo)
 long unsigned int *lui_1d_allocate(const long unsigned int N){
     long unsigned int *A;
-    A     = (long unsigned int*)malloc((N)*sizeof(long unsigned int));
-    memset(A, 0, sizeof(long unsigned int)*N);
+    A     = (long unsigned int*)calloc((N),sizeof(long unsigned int));
     return A;
 }
 
@@ -69,8 +67,7 @@ void free_lui_1d_allocate(long unsigned int *A){
 /// \author Kazuyoshi Yoshimi (University of Tokyo)
 long int *li_1d_allocate(const long unsigned int N){
     long int *A;
-    A     = (long  int*)malloc((N)*sizeof(long int));
-    memset(A, 0, sizeof(long int)*N);
+    A     = (long  int*)calloc((N),sizeof(long int));
     return A;
 }
 
@@ -90,12 +87,12 @@ void free_li_1d_allocate(long int *A){
 long int **li_2d_allocate(const long unsigned int N, const long unsigned int M) {
     long int **A;
     long unsigned int int_i;
-    A = (long int **) malloc((N) * sizeof(long int *));
-    A[0] = (long int *) malloc((M * N) * sizeof(long int));
+    A = (long int **) calloc((N) , sizeof(long int *));
+    A[0] = (long int *) calloc((M * N) ,sizeof(long int));
     for (int_i = 0; int_i < N; int_i++) {
         A[int_i] = A[0] + int_i * M;
     }
-    memset(A[0], 0, sizeof(long int)*M*N);
+    //memset(A[0], 0, sizeof(long int)*M*N);
     return A;
 }
 ///
@@ -115,8 +112,8 @@ void free_li_2d_allocate(long int **A){
 /// \author Kazuyoshi Yoshimi (University of Tokyo)
 int *i_1d_allocate(const long unsigned int N){
     int *A;
-    A     = (int*)malloc((N)*sizeof(int));
-    memset(A, 0, sizeof(int)*N);
+    A     = (int*)calloc((N),sizeof(int));
+    //memset(A, 0, sizeof(int)*N);
     return A;
 }
 
@@ -137,12 +134,12 @@ void free_i_1d_allocate(int *A){
 int **i_2d_allocate(const long unsigned int N, const long unsigned int M) {
     int **A;
     long unsigned int int_i;
-    A = (int **) malloc((N) * sizeof(int *));
-    A[0] = (int *) malloc((M * N) * sizeof(int));
+    A = (int **) calloc((N) , sizeof(int *));
+    A[0] = (int *) calloc((M * N) , sizeof(int));
     for (int_i = 0; int_i < N; int_i++) {
         A[int_i] = A[0] + int_i * M;
     }
-    memset(A[0], 0, sizeof(int)*M*N);
+    //memset(A[0], 0, sizeof(int)*M*N);
     return A;
 }
 ///
@@ -161,16 +158,15 @@ void free_i_2d_allocate(int **A){
 int***i_3d_allocate(const long unsigned int N, const long unsigned int M, const long unsigned int L){
     long unsigned int int_i, int_j;
     int*** A;
-    A     = (int***)malloc((N)*sizeof(int**));
-    A[0]  = (int**)malloc((M*N)*sizeof(int*));
-    A[0][0] = (int*)malloc((L*M*N)*sizeof(int));
+    A     = (int***)calloc((N),sizeof(int**));
+    A[0]  = (int**)calloc((M*N),sizeof(int*));
+    A[0][0] = (int*)calloc((L*M*N),sizeof(int));
     for(int_i=0;int_i<N; int_i++) {
         A[int_i] = A[0] + int_i*M;
         for(int_j = 0; int_j<M; int_j++){
             A[int_i][int_j]= A[0][0] + int_i*M*L + int_j*L;
         }
     }
-    memset(A[0][0], 0, sizeof(int)*L*M*N);
     return A;
 }
 
@@ -191,8 +187,7 @@ void free_i_3d_allocate(int ***A){
 /// \author Kazuyoshi Yoshimi (University of Tokyo)
 double *d_1d_allocate(const long unsigned int N){
     double *A;
-    A     = (double*)malloc((N)*sizeof(double));
-    memset(A, 0.0, sizeof(double)*N);
+    A     = (double*)calloc((N),sizeof(double));
     return A;
 }
 ///
@@ -212,12 +207,11 @@ void free_d_1d_allocate(double *A){
 double **d_2d_allocate(const long unsigned int N, const long unsigned int M){
     long unsigned int int_i;
     double **A;
-    A     = (double**)malloc((N)*sizeof(double*));
-    A[0]  = (double*)malloc((M*N)*sizeof(double));
+    A     = (double**)calloc((N),sizeof(double*));
+    A[0]  = (double*)calloc((M*N),sizeof(double));
     for(int_i=0;int_i<N;int_i++){
         A[int_i] = A[0] + int_i*M;
     }
-    memset(A[0], 0.0, sizeof(double)*M*N);
     return A;
 }
 
@@ -237,8 +231,7 @@ void free_d_2d_allocate(double **A){
 /// \author Kazuyoshi Yoshimi (University of Tokyo)
 double complex *cd_1d_allocate(const long unsigned int N){
     double complex*A;
-    A     = (double complex*)malloc((N)*sizeof(double complex));
-    memset(A, 0.0, sizeof(double complex)*N);
+    A     = (double complex*)calloc((N),sizeof(double complex));
     return A;
 }
 ///
@@ -258,12 +251,11 @@ void free_cd_1d_allocate(double complex *A){
 complex double **cd_2d_allocate(const long unsigned int N, const long unsigned int M){
     long unsigned int int_i;
     complex double **A;
-    A     = (complex double**)malloc((N)*sizeof(complex double));
-    A[0]  = (complex double*)malloc((M*N)*sizeof(complex double));
+    A     = (complex double**)calloc((N),sizeof(complex double));
+    A[0]  = (complex double*)calloc((M*N),sizeof(complex double));
     for(int_i=0;int_i<N;int_i++){
         A[int_i] = A[0]+int_i*M;
     }
-    memset(A[0], 0.0, sizeof(double complex)*M*N);
     return A;
 }
 
@@ -283,16 +275,15 @@ void free_cd_2d_allocate(double complex**A){
 double complex***cd_3d_allocate(const long unsigned int N, const long unsigned int M, const long unsigned int L){
     long unsigned int int_i, int_j;
     double complex***A;
-    A     = (double complex***)malloc((N)*sizeof(double complex**));
-    A[0]  = (double complex**)malloc((M*N)*sizeof(double complex*));
-    A[0][0] = (double complex*)malloc((L*M*N)*sizeof(double complex));
+    A     = (double complex***)calloc((N),sizeof(double complex**));
+    A[0]  = (double complex**)calloc((M*N),sizeof(double complex*));
+    A[0][0] = (double complex*)calloc((L*M*N),sizeof(double complex));
     for(int_i=0;int_i<N; int_i++) {
         A[int_i] = A[0] + int_i*M;
         for(int_j = 0; int_j<M; int_j++){
             A[int_i][int_j]= A[0][0] + int_i*M*L + int_j*L;
         }
     }
-    memset(A[0][0], 0.0, sizeof(double complex)*L*M*N);
     return A;
 }
 
