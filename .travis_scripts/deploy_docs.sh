10c66769a87b9400bba17a6339021024af7d94e1
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2020-01-14 18:33:59 +0900

    fixed deploy script

--- .travis_scripts/deploy_docs.sh
+++ .travis_scripts/deploy_docs.sh
@@ -26,7 +26,7 @@ sudo apt-get install -y texlive-latex-recommended texlive-latex-extra texlive-la
 kanji-config-updmap-sys ipaex
 sudo pip install sphinx sphinxcontrib.spelling
 
-openssl aes-256-cbc -K $encrypted_87f43018402c_key -iv $encrypted_87f43018402c_iv -in ${ROOTDIR}/id_rsa.enc -out ~/.ssh/id_rsa -d
+openssl aes-256-cbc -K $encrypted_87f43018402c_key -iv $encrypted_87f43018402c_iv -in ${ROOTDIR}/.travis_scripts/id_rsa.enc -out ~/.ssh/id_rsa -d
 
 chmod 600 ~/.ssh/id_rsa
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
