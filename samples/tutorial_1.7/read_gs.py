e1f529268b787d85593dfcbdc32850be07870904
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2020-05-22 16:21:39 +0900

    mod tutorial 1.7

--- samples/tutorial_1.7/read_gs.py
+++ samples/tutorial_1.7/read_gs.py
@@ -1,11 +1,10 @@
-def read_gs(*, index=0, rank=0):
-    import numpy as np
-    from os.path import join
-    from struct import unpack
+import numpy as np
+from os.path import join
+from struct import unpack
 
-    filename = join("output",
-                    "zvo_eigenvec_{}_rank_{}.dat".format(index,
-                                                         rank))
+
+def read_gs(*, index=0, rank=0):
+    filename = join("output", "zvo_eigenvec_{}_rank_{}.dat".format(index, rank))
     with open(filename, "rb") as f:
         f.read(4)
         nelems = unpack("L", f.read(8))[0]
@@ -16,6 +15,3 @@ def read_gs(*, index=0, rank=0):
             im = unpack("d", f.read(8))[0]
             ret[i] = np.complex(re, im)
         return ret
-
-vec = read_gs()
-#print(vec)
--- samples/tutorial_1.7/solution.py
+++ samples/tutorial_1.7/solution.py
@@ -3,6 +3,8 @@ import subprocess
 import numpy as np
 from numpy.linalg import norm
 
+import read_gs
+
 
 def create_input():
     with open("std.in", "w") as f:
@@ -17,31 +19,14 @@ def create_input():
         print('eigenvecio = "out"', file=f)
 
 
-def read_gs(*, exct=0, rank=0):
-    from os.path import join
-    from struct import unpack
-
-    filename = join("output", "zvo_eigenvec_{}_rank_{}.dat".format(exct, rank))
-    with open(filename, "rb") as f:
-        f.read(4)
-        nelems = unpack("L", f.read(8))[0]
-        ret = np.zeros(nelems, dtype=np.complex)
-        f.read(16)
-        for i in range(nelems):
-            re = unpack("d", f.read(8))[0]
-            im = unpack("d", f.read(8))[0]
-            ret[i] = np.complex(re, im)
-        return ret
-
-
 if __name__ == "__main__":
     create_input()
     cmd = ["HPhi", "-s", "std.in"]
     subprocess.call(cmd)
     print()
 
-    gs = read_gs()
-    exc = read_gs(exct=1)
+    gs = read_gs.read_gs()
+    exc = read_gs.read_gs(index=1)
 
     print("norm of gs    = ", norm(gs))
     print("norm of exc   = ", norm(exc))
