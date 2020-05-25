bd0c74fe9dec77f4f4c34a86c04920e497dd58b2
k-yoshimi <k-yoshimi@issp.u-tokyo.ac.jp>
2020-05-25 17:39:12 +0900

    update test for tutorial

--- samples/do_all.py
+++ samples/do_all.py
@@ -1,25 +1,38 @@
 import os
+import sys
 import subprocess
 import glob
+import time
+
+#Set path(command) to Hphi
+if len(sys.argv) == 2:
+    path_to_HPhi = os.path.abspath(sys.argv[1])
+else:
+    print("Error")
+    print("Usage: python do_all.py path_to_HPhi.")
+    print("path_to_HPhi: relative or absolute path to HPhi.")
+    exit(1)
+
 
 #get all dictioary of tutorials
-dir_all = glob.glob("tutorial*")
-path_to_HPhi = "/Users/k-yoshimi/program/HPhi_box/build/src/HPhi"
+dir_all = sorted(glob.glob("tutorial*"))
 current_dir = os.getcwd()
-for dir_name in dir_all:
+for idx, dir_name in enumerate(dir_all):
+    print("#########Test {}/{}#############".format(idx+1, len(dir_all)))
     print("Check: {}".format(dir_name))
     dir_name = os.path.join(current_dir, dir_name) 
     os.chdir(dir_name)
     #Run script
     if os.path.exists("run.sh"):
         print("Start calculation.")
+        start = time.time()
         with open("run.sh", "r") as fr:
             lines = fr.readlines()
             for line in lines[1:]:
                 line = line.replace("$1", path_to_HPhi)
                 cmd = line.split()
+                print(cmd)
                 subprocess.call(cmd, stdout=subprocess.PIPE)
-        print("Check file.")
         with open("check_file.txt", "r") as fr:
             lines = fr.readlines()
             bool_test = True
@@ -28,8 +41,9 @@ for dir_name in dir_all:
                 if path_to_file is False:
                     bool_test = False
                     print("Error: {} does not exist".format(line.strip()))
+        elapsed_time = time.time() - start
+        print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
         if bool_test is True:
             print("Success to do {}".format(dir_name))
-        print("Finish calculation")
     else:
         print("Error: run.sh does not exist in {}".format(dir_name))
