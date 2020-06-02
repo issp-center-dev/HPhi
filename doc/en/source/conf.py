efbcb3c2c9ce961c6bb824929751367ead764985
k-yoshimi <k-yoshimi@issp.u-tokyo.ac.jp>
2020-06-02 15:43:54 +0900

    mv samples/old/Wannier

--- doc/en/source/conf.py
+++ doc/en/source/conf.py
@@ -30,7 +30,7 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.mathjax', 'sphinxcontrib.spelling']
+extensions = ['sphinx.ext.mathjax']
 mathjax_pass = ['https://cdnjs.com/']
 math_number_all = True
 spelling_lang='en_US'
