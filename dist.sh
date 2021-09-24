eead60463a7096b4151ddae3266250c2b1488cb5
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2021-09-24 14:07:47 +0900

    update dist.sh to use git-archive-all tool

--- dist.sh
+++ dist.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 #
-# #############  Note  ################################
-# Before packing, you should clean the GIT directory as
-# $ git clean -f -d -x
-# #####################################################
+# #############  README  ##############
+# This makes an archive file including static copy of submodules (StdFace)
+# and PDF formatted documents docs/userguide_HPhi_(ja|en).pdf
+# The output filename is HPhi-${vid}.tar.gz,
+# where ${vid} is the version number such as 3.5.0 .
+# Before using this, install the following python packages:
+#   sphinx
+#   sphinx_numfig
+#   sphinxcontib_spelling
+#   git-archive-all
+# #####################################
 #
 # Version ID
 #
@@ -11,29 +18,21 @@ major=`cat src/include/version_major.h`
 minor=`cat src/include/version_minor.h`
 patch=`cat src/include/version_patch.h`
 vid=`echo ${major}.${minor}.${patch}`
-#
-mkdir HPhi-${vid}
-#
-#cp -rf * HPhi-${vid}
-rsync --exclude HPhi-${vid} -a * HPhi-${vid}
-#
+
+ROOTDIR=`pwd`
+
 # Build docments
-#
-cd HPhi-${vid}/doc/ja
+cd doc/ja
 make latexpdf
-cp ./build/latex/userguide_HPhi_ja.pdf ../../
+cp ./build/latex/userguide_HPhi_ja.pdf $ROOTDIR/doc
 cd ../en
 make latexpdf
-cp ./build/latex/userguide_HPhi_en.pdf ../../
-#
-# Remove some files
-#
-find ./ -name ".git*" -delete
-#
-# Pack
-#
-cd ../../
-rm dist.sh
-cd ../
-tar czvf HPhi-${vid}.tar.gz HPhi-${vid}
-rm -rf HPhi-${vid}
+cp ./build/latex/userguide_HPhi_en.pdf $ROOTDIR/doc
+
+# Make a tarball
+cd $ROOTDIR
+git-archive-all \
+  --extra=doc/userguide_HPhi_ja.pdf \
+  --extra=doc/userguide_HPhi_en.pdf \
+  --prefix=HPhi-${vid} \
+  HPhi-${vid}.tar.gz
