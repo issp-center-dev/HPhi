a73f6b74b4a1483fcc6b19f3346e3f3a895c63e3
HPhi Developers <qlms-dev@issp.u-tokyo.ac.jp>
2021-09-28 00:42:40 +0000

    Deploy docs to develop by GitHub Actions triggered by c389ce2c0d0bdb4611fae34b27333cf854439503

--- manual/develop/ja/html/_static/documentation_options.js
+++ manual/develop/ja/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '',
+    VERSION: '3.5.0',
     LANGUAGE: 'ja',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
