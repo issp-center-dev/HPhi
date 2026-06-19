5d6dc7aa91a463be062640f6279d38e02d200f0b
HPhi Developers <qlms-dev@issp.u-tokyo.ac.jp>
2026-06-19 01:29:33 +0000

    Deploy docs to develop by GitHub Actions triggered by a334ec4d7bea0e9b8018b15776cd2d22237e433b

--- manual/develop/api/search/functions_c.js
+++ manual/develop/api/search/functions_c.js
@@ -1,8 +1,14 @@
 var searchData=
 [
-  ['nbody_5frank_5fflip_5fmask_0',['nbody_rank_flip_mask',['../nbody__interall_8c.html#ab8c04f62f582fd9e340fb24f80aa50e6',1,'nbody_interall.c']]],
-  ['nbodyg_5frank_5fflip_5fmask_1',['nbodyg_rank_flip_mask',['../nbody__correlation_8c.html#ae7635417d235cd286d377b67cf15fe54',1,'nbody_correlation.c']]],
-  ['normalizenbodygterms_2',['normalizenbodygterms',['../nbody__correlation_8h.html#abc1d3af5e3fba97bca45b5ee6dafaf8b',1,'NormalizeNBodyGTerms(struct DefineList *D):&#160;nbody_correlation.c'],['../nbody__correlation_8c.html#abc1d3af5e3fba97bca45b5ee6dafaf8b',1,'NormalizeNBodyGTerms(struct DefineList *D):&#160;nbody_correlation.c']]],
-  ['normalizenbodyinterallterms_3',['normalizenbodyinterallterms',['../nbody__interall_8h.html#ad6740df54518a20e38a5a0ee5914b97c',1,'NormalizeNBodyInterAllTerms(struct DefineList *D):&#160;nbody_interall.c'],['../nbody__interall_8c.html#ad6740df54518a20e38a5a0ee5914b97c',1,'NormalizeNBodyInterAllTerms(struct DefineList *D):&#160;nbody_interall.c']]],
-  ['normmpi_5fdc_4',['normmpi_dc',['../wrapperMPI_8h.html#a26d9f2a4d2828fca2b8d6d45cea697a2',1,'NormMPI_dc(unsigned long int idim, double complex *_v1):&#160;wrapperMPI.c'],['../wrapperMPI_8c.html#a26d9f2a4d2828fca2b8d6d45cea697a2',1,'NormMPI_dc(unsigned long int idim, double complex *_v1):&#160;wrapperMPI.c']]]
+  ['nbody_5finterall_5fgeneral_5fspin_5fpartner_5frank_0',['nbody_interall_general_spin_partner_rank',['../nbody__interall_8c.html#a673d20db56b47ef6aaaa56a24e41339a',1,'nbody_interall.c']]],
+  ['nbody_5fis_5fgeneral_5fspin_1',['nbody_is_general_spin',['../nbody__interall_8c.html#af322662f8b8ea7c160f57825af34ca4d',1,'nbody_interall.c']]],
+  ['nbody_5fis_5fsupported_5fspin_5fmodel_2',['nbody_is_supported_spin_model',['../nbody__interall_8c.html#a7b294ffb9af3b5c960cbca32cefe3731',1,'nbody_interall.c']]],
+  ['nbody_5frank_5fflip_5fmask_3',['nbody_rank_flip_mask',['../nbody__interall_8c.html#ab8c04f62f582fd9e340fb24f80aa50e6',1,'nbody_interall.c']]],
+  ['nbodyg_5fgeneral_5fspin_5fpartner_5frank_4',['nbodyg_general_spin_partner_rank',['../nbody__correlation_8c.html#a7dce93f6f6130dc87f02b00a7235045d',1,'nbody_correlation.c']]],
+  ['nbodyg_5fis_5fgeneral_5fspin_5',['nbodyg_is_general_spin',['../nbody__correlation_8c.html#aabbdf4ba64486cc637bf0de75e3307c5',1,'nbody_correlation.c']]],
+  ['nbodyg_5fis_5fsupported_5fspin_5fmodel_6',['nbodyg_is_supported_spin_model',['../nbody__correlation_8c.html#a4d2c36f33ea2ba5ab455029e4af87861',1,'nbody_correlation.c']]],
+  ['nbodyg_5frank_5fflip_5fmask_7',['nbodyg_rank_flip_mask',['../nbody__correlation_8c.html#ae7635417d235cd286d377b67cf15fe54',1,'nbody_correlation.c']]],
+  ['normalizenbodygterms_8',['normalizenbodygterms',['../nbody__correlation_8h.html#abc1d3af5e3fba97bca45b5ee6dafaf8b',1,'NormalizeNBodyGTerms(struct DefineList *D):&#160;nbody_correlation.c'],['../nbody__correlation_8c.html#abc1d3af5e3fba97bca45b5ee6dafaf8b',1,'NormalizeNBodyGTerms(struct DefineList *D):&#160;nbody_correlation.c']]],
+  ['normalizenbodyinterallterms_9',['normalizenbodyinterallterms',['../nbody__interall_8h.html#ad6740df54518a20e38a5a0ee5914b97c',1,'NormalizeNBodyInterAllTerms(struct DefineList *D):&#160;nbody_interall.c'],['../nbody__interall_8c.html#ad6740df54518a20e38a5a0ee5914b97c',1,'NormalizeNBodyInterAllTerms(struct DefineList *D):&#160;nbody_interall.c']]],
+  ['normmpi_5fdc_10',['normmpi_dc',['../wrapperMPI_8h.html#a26d9f2a4d2828fca2b8d6d45cea697a2',1,'NormMPI_dc(unsigned long int idim, double complex *_v1):&#160;wrapperMPI.c'],['../wrapperMPI_8c.html#a26d9f2a4d2828fca2b8d6d45cea697a2',1,'NormMPI_dc(unsigned long int idim, double complex *_v1):&#160;wrapperMPI.c']]]
 ];
