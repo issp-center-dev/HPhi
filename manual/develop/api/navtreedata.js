91160e2b5faa67701475fbd3eb6b962e2301f8bc
HPhi Developers <qlms-dev@issp.u-tokyo.ac.jp>
2026-06-19 05:26:17 +0000

    Deploy docs to develop by GitHub Actions triggered by c4b007bee33d0623daa01045c2cdb46a9c88c655

--- manual/develop/api/navtreedata.js
+++ manual/develop/api/navtreedata.js
@@ -148,9 +148,9 @@ var NAVTREEINDEX =
 "global_8h.html#af964b432d6b4264b8f3e88ec42754730",
 "mltplyMPIBatched_8c.html#a729d4d12be5404b5148c84ba03123f71",
 "mltplySpinCore_8c.html#a34add9562270d08b67c2f8479d02e5e4",
-"page_codingrule.html#subsec_variables",
-"structDefineList.html#a1beb614fe91643dbf2d4c9a941973d91",
-"structMPITransferGroup.html#a9c421c95245ae78449b9b4ffc00014e5"
+"page_addmodpara.html#subsec_modpara_step4",
+"structDefineList.html#a09fa2f50f7daaa648426f773af18e18d",
+"structMPISpinExchangeGroup.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
--- manual/develop/api/nbody__interall_8h.js
+++ manual/develop/api/nbody__interall_8h.js
@@ -1,15 +1,19 @@
 var nbody__interall_8h =
 [
+    [ "AddNBodyInterAllToHamHubbard", "nbody__interall_8h.html#aab23d376b0972153aa8c392ce15bfcf4", null ],
     [ "AddNBodyInterAllToHamHubbardGC", "nbody__interall_8h.html#a54b87f352317037a3966b2a1673d4f3d", null ],
     [ "AddNBodyInterAllToHamSpinGC", "nbody__interall_8h.html#a792e02152c230b226b0a6f60c9747664", null ],
     [ "ApplyNBodyInterAllSpinGC", "nbody__interall_8h.html#a43959f8ddcfac4b77c4268fd220caf06", null ],
     [ "CheckNBodyInterAllHermitePairs", "nbody__interall_8h.html#a2e58f420a94e4851cdeacd9141892d38", null ],
+    [ "CheckNBodyInterAllHubbardConservation", "nbody__interall_8h.html#a69eb1909a531821351fd147621044f87", null ],
     [ "CheckNBodyInterAllSpinConservation", "nbody__interall_8h.html#a265de1dd948547b3050e2b2d528acd7d", null ],
     [ "ClassifyNBodyInterAllTerms", "nbody__interall_8h.html#a0d0f4d9c25724773dd9ea3e6d9c1de23", null ],
+    [ "MultiplyNBodyInterAllHubbard", "nbody__interall_8h.html#ac4e7a02c4a9fd0726f34dc73ed7cd7ce", null ],
     [ "MultiplyNBodyInterAllHubbardGC", "nbody__interall_8h.html#a3b46d23abca147529162d9c63807bb5a", null ],
     [ "MultiplyNBodyInterAllSpinGC", "nbody__interall_8h.html#a60329423c36e350c0635e9fc4a9b132f", null ],
     [ "NormalizeNBodyInterAllTerms", "nbody__interall_8h.html#ad6740df54518a20e38a5a0ee5914b97c", null ],
     [ "ParseNBodyInterAllLine", "nbody__interall_8h.html#a99172b55b2d479dc2a1a2fdde546656a", null ],
+    [ "SetDiagonalNBodyInterAllHubbard", "nbody__interall_8h.html#a72ce8d20f8e71582b6a3989dad0bd8d7", null ],
     [ "SetDiagonalNBodyInterAllHubbardGC", "nbody__interall_8h.html#a38d9758905af6a83d5a9df36225e9c7f", null ],
     [ "SetDiagonalNBodyInterAllSpinGC", "nbody__interall_8h.html#a781b6943ee9ea59f8fc902d5db5e35cd", null ],
     [ "ValidateNBodyInterAllScope", "nbody__interall_8h.html#aa2fd89bc2da06b94f6c17e00a7b963d6", null ]
